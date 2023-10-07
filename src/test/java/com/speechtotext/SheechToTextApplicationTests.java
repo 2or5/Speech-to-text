29b4a23d6703e9c7d71deed442b4f95bc2dbf825
Nazar <nazarbokalo0@gmail.com>
2023-10-07 13:04:02 +0300

    connect Junit 5

--- src/test/java/com/speechtotext/SheechToTextApplicationTests.java
+++ src/test/java/com/speechtotext/SheechToTextApplicationTests.java
@@ -1,9 +1,7 @@
 package com.speechtotext;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class SheechToTextApplicationTests {
 
     @Test
