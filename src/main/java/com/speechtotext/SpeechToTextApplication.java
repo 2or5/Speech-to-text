983a0a0d398096c62ea20976a759b4451707dc9b
2or5 <nazarbokalo0@gmail.com>
2023-11-18 14:37:37 +0200

    add google cloud api

--- src/main/java/com/speechtotext/SpeechToTextApplication.java
+++ src/main/java/com/speechtotext/SpeechToTextApplication.java
@@ -1,13 +1,46 @@
 package com.speechtotext;
 
+import com.google.cloud.speech.v1.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.util.List;
 
 @SpringBootApplication
 public class SpeechToTextApplication {
 
     public static void main(String[] args) {
+        System.out.println(System.getenv("GOOGLE_APPLICATION_CREDENTIALS"));
         SpringApplication.run(SpeechToTextApplication.class, args);
-    }
 
+        // Instantiates a client
+        try (SpeechClient speechClient = SpeechClient.create()) {
+
+            // The path to the audio file to transcribe
+            String gcsUri = "gs://wgebrehnbrethnj4retn/test.mp3";
+
+            // Builds the sync recognize request
+            RecognitionConfig config =
+                    RecognitionConfig.newBuilder()
+                            .setEncoding(RecognitionConfig.AudioEncoding.MP3)
+                            .setSampleRateHertz(16000)
+                            .setLanguageCode("en-US")
+                            .build();
+            RecognitionAudio audio = RecognitionAudio.newBuilder().setUri(gcsUri).build();
+
+            // Performs speech recognition on the audio file
+            RecognizeResponse response = speechClient.recognize(config, audio);
+            List<SpeechRecognitionResult> results = response.getResultsList();
+
+            for (SpeechRecognitionResult result : results) {
+                // There can be several alternative transcripts for a given chunk of speech. Just use the
+                // first (most likely) one here.
+                SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
+                System.out.printf("Transcription: %s%n", alternative.getTranscript());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
